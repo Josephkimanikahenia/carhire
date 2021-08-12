73692e8f589a2f7c0bebd5a9c7e8a86a19fa81da
joseph <kkahenia@gmail.com>
2021-08-12 19:55:22 +0300

    model changes

--- cars/models.py
+++ cars/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
-#from ckeditor.fields import RichTextField
-#from multiselectfield import MultiSelectField
+from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 
