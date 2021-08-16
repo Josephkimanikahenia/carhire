02fa768aa8b5527e3c25b0c36c271481c2df9880
joseph <kkahenia@gmail.com>
2021-08-16 21:58:43 +0300

    settings

--- carhire/settings.py
+++ carhire/settings.py
@@ -31,14 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cars.apps.CarsConfig',
-    'pages.apps.PagesConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+  
 ]
 
 MIDDLEWARE = [
